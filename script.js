70e3998200be10b0ef5363275fa26cde9b5ebea7
David <drgdeved@gmail.com>
2025-07-07 20:50:07 +0300

    Add getComputerChoice to generate a random computer play

--- script.js
+++ script.js
@@ -10,11 +10,19 @@
 */
 
 function getHumanChoice() {
-    humanChoice = prompt("What's your play: rock, paper, scissors");
+    let humanChoice = prompt("What's your play: rock, paper, scissors");
     if(["rock", "paper", "scissors"].includes(humanChoice.toLowerCase())) {
         return humanChoice;
     }
     else getHumanChoice();
 }
 
-
+function getComputerChoice() {
+    let number = (Math.random() * 10);
+    let computerChoice = number => {
+        if (number <= 3.33) return "rock";
+        else if (number > 3.33 && number <= 6.66) return "scissors";
+        else return "paper";
+    };
+    return computerChoice(number);
+}
