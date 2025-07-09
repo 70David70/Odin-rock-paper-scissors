f3faddf504b0a47abbf803175918340eb920818b
David <drgdeved@gmail.com>
2025-07-09 21:13:07 +0300

    removed some unnecessary comments

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-/*
-    1- GET INPUT
-    2- check if the input a string
-        if not ask again
-    3- check if the input one of the choises
-        if not ask again
-    4- generate a random play
-    5- compare the player and the bot's play and decide who wins
-    6- if they have the same play ask for input again
-*/
 
 function getHumanChoice() {
     let humanChoice = prompt("What's your play: rock, paper, scissors");
